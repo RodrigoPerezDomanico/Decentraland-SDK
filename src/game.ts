9037f9b1938890af4e97fb64ab4e1181c84fcd0a
Rodrigo Perez Domanico <rodrigoperez33@hotmail.com>
2022-09-02 13:28:42 -0300

    Started SDK

---
+++ src/game.ts
@@ -0,0 +1,9 @@
+import { createDispenser } from './booth/dispenser'
+
+createDispenser(
+  {
+    position: new Vector3(8, 0, 8),
+    rotation: Quaternion.Euler(0, 0, 0)
+  },
+  'acd27e4b-24bd-4040-b715-c0e11e863fb0'
+)
